ad9d9e826a97dc6ac704f3f37902da691f1e56ea
Neil Larkin <47116610+neillarkin@users.noreply.github.com>
2020-09-20 11:09:04 +0100

    Added IF Statments to handle false searches. Chnaged URL variable names.

--- app/app.py
+++ app/app.py
@@ -1,11 +1,12 @@
 import os
-from flask import Flask, request, render_template, url_for, jsonify, redirect
+from flask import Flask, render_template, request, url_for
 import requests, json
 
 app = Flask(__name__)
-url = "https://pokeapi.co/api/v2/pokemon/"
-url2 = "https://pokeapi.co/api/v2/pokemon-species/"
-url3 = "https://api.funtranslations.com/translate/shakespeare.json?text="
+
+POKEMON_API_URL = "https://pokeapi.co/api/v2/pokemon/"
+POKEMON_SPECIES_API_URL = "https://pokeapi.co/api/v2/pokemon-species/"
+TRANSLATION_API_URL = "https://api.funtranslations.com/translate/shakespeare.json?text="
 
 
 @app.route('/')
@@ -14,19 +15,30 @@ def index():
 
 @app.route('/pokemon', methods=['GET', 'POST'])
 def pokemon():
+    error = None
+    description = None
     query = request.args['search']
-    result = url + query
-    response1 = requests.get(result)
-    dict = response1.json()
-    name = str(dict['name'])
-    result2 = url2 + name
-    response2 = requests.get(result2)
-    dict.clear()
-    dict = response2.json()
-    flavor_entries = dict ['flavor_text_entries']
-    flavor_text = flavor_entries[0]
-    description = str(flavor_text['flavor_text'])
-    return render_template('index.html', name=name, result=result, result2=result2, description=description)
+    query_url = POKEMON_API_URL + query
+
+    if not query:
+        error = "Enter a character name"
+    elif query:
+        query_response = requests.get(query_url)
+        if query_response.status_code != 200:
+            error = str(query_response.status_code) + " Nothing Found"
+        else:
+            dict = query_response.json()
+            error = str(dict['name'])
+            query_url = POKEMON_SPECIES_API_URL + error
+            query_response = requests.get(query_url)
+            dict.clear()
+            dict = query_response.json()
+            flavor_entries = dict['flavor_text_entries']
+            flavor_text = flavor_entries[0]
+            description = str(flavor_text['flavor_text'])
+
+    return render_template('index.html', error=error, description=description)
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
